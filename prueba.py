a63ead890a40bc25d264fc3729c78729295901dd
Frankilles <franko.rivas@genstudents.org>
2024-11-26 16:01:07 +0000

    first commit

---
+++ prueba.py
@@ -0,0 +1,6 @@
+
+
+nombre = input ("Ingresa tu nombre: ") #imprime un mensaje al usuario y espera que ingrese un dato
+apellido = input ("Ingresa tu apellido: ")
+ciudad = input ("Ingrsa tu ciudad: ")
+print(f"Hola, {nombre} {apellido} de la ciudad {ciudad}. Bienvenid@ a nuestra app.") 
